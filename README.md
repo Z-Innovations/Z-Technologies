89f0bca3715d61f534d7ddf39d71ffbd2a18b06d
roundedrectangle <kozelderezel12345678@gmail.com>
2024-12-30 00:07:36 +0300

    better examples and readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# Touch the future with Z-Innovations!
+
+### For rus users (in other words all users)
+
+# Прикоснись к будущему с Z-Инновациями!
--- examples/api.py
+++ examples/api.py
@@ -1,17 +1,18 @@
 import json
 import requests
 
+api_url = 'http://127.0.0.1:5000' # change if required
 register = False
 
 with requests.Session() as s:
     name, password = input("name>"), input("password>")
 
     if register:
-        s.post('http://127.0.0.1:5000/register', data={'name': name, 'password': password})
+        s.post(f'{api_url}/register', data={'name': name, 'password': password})
     else:
-        s.post('http://127.0.0.1:5000/login', data={'name': name, 'password': password})
+        s.post(f'{api_url}/login', data={'name': name, 'password': password})
 
-    res = s.get('http://127.0.0.1:5000/api')
+    res = s.get(f'{api_url}/api')
     res = json.loads(res.text)
     print('name:', res['name'])
     print('discriminator:', res['discriminator'])
