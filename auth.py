80fe16a151e8cf7d98461f158afa284b50db031e
roundedrectangle <kozelderezel12345678@gmail.com>
2024-12-30 10:54:18 +0300

    auth blueprint, templates

--- auth.py
+++ auth.py
@@ -1,15 +1,11 @@
-from flask_login import UserMixin, LoginManager
-import sqlite3
+from flask_login import UserMixin, LoginManager, current_user, login_user, login_required, logout_user
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from flask import Blueprint, request, render_template, redirect, url_for
+from werkzeug.security import check_password_hash, generate_password_hash
 
 class Base(DeclarativeBase):
     pass
-
-# class User(UserMixin):
-#     def __init__(self, id: str | None = None) -> None:
-#         self.id = id or None
-#         super().__init__()
 
 class MyLoginManager(LoginManager):
     def __init__(self, app=None, add_context_processor=True):
@@ -21,27 +17,67 @@ class MyLoginManager(LoginManager):
             password: Mapped[str]
         self.User = User
 
-        #self.con = sqlite3.connect(db)
-        #self.cur = self.con.cursor()
-        #self.cur.execute('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT NOT NULL, password TEXT NOT NULL)')
+        self.bp = Blueprint('auth', __name__, url_prefix='/auth')
+        self.bp.route('/register', methods=('GET', 'POST'))(self.register)
+        self.bp.route('/unregister', methods=('GET', 'POST'))(self.unregister)
+        self.bp.route('/login', methods=('GET', 'POST'))(self.login)
+        self.bp.route('/logout')(self.logout)
 
         super().__init__(app, add_context_processor)
         self.user_loader(self._user_loader)
-        # self.request_loader(self._request_loader)
 
     def _user_loader(self, id):
         try:
             return self.User.query.get(int(id))
-        except:
-            return
-
-    # def _request_loader(self, request):
-    #     name = request.form.get('id')
-    #     if self.cur.execute('SELECT name FROM users WHERE name = ?', name) is not None:
-    #         return User(name)
+        except: pass
 
     def init_app(self, app, add_context_processor=True):
         self.db.init_app(app)
         with app.app_context():
             self.db.create_all()
         return super().init_app(app, add_context_processor)
+
+    def register(self):
+        match request.method:
+            case 'GET':
+                return render_template('login_register.html', mode=0)
+            case 'POST':
+                name, password = request.form['name'], request.form['password']
+                user = self.User.query.filter_by(name=name).first()
+                if not user and name and password:
+                    user = self.User(name=name, password=generate_password_hash(password))
+                    self.db.session.add(user)
+                    self.db.session.commit()
+                    login_user(user)
+                    return redirect(url_for('protected'))
+        return 'something went wrong'
+
+    @login_required
+    def unregister(self):
+        match request.method:
+            case 'GET':
+                return render_template('login_register.html', mode=2)
+            case 'POST':
+                password = request.form['password']
+                if check_password_hash(current_user.password, password):
+                    self.db.session.delete(current_user)
+                    self.db.session.commit()
+                    logout_user()
+                    return redirect('/')
+        return '<h1>something went wrong</h1>\n<p>Wrong password?</p>'
+
+    def login(self):
+        if request.method == 'GET':
+            return render_template('login_register.html', mode=1)
+
+        name, password = request.form['name'], request.form['password']
+        user = self.User.query.filter_by(name=name).first()
+        if user and check_password_hash(user.password, password):
+            login_user(user)
+            return redirect(url_for('protected'))
+
+        return 'Bad login'
+
+    def logout(self):
+        logout_user()
+        return 'Logged out'
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, url_for
+from flask import Flask, request, redirect, url_for, render_template
 from markupsafe import escape
 from _secrets import SECRET_KEY
 from auth import MyLoginManager
@@ -13,39 +13,16 @@ app.secret_key = SECRET_KEY
 mgr = MyLoginManager(app)
 User = mgr.User
 
+app.register_blueprint(mgr.bp)
+
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    return render_template('index.html')
 
-# @app.route("/<name>")
-# def test(name):
-#     # escape() fixes stuff like <script>alert("VIRUS")</script> to be passed as the name
-#     return f"Signed in as {escape(name)}"
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'GET':
-        return '''
-               <form action='login' method='POST'>
-                <input type='text' name='name' id='name' placeholder='name'/>
-                <input type='password' name='password' id='password' placeholder='password'/>
-                <input type='submit' name='submit'/>
-               </form>
-               '''
-
-    name, password = request.form['name'], request.form['password']
-    user = User.query.filter_by(name=name).first()
-    if user and check_password_hash(user.password, password):
-        login_user(user)
-        return redirect(url_for('protected'))
-
-    return 'Bad login'
-
-
-@app.route('/protected')
+@app.route('/me')
 @login_required
 def protected():
-    return f'Logged in as: {escape(current_user.name)}#{current_user.id}'
+    return render_template('me.html')
 
 @app.route('/api')
 @login_required
@@ -55,58 +32,6 @@ def api():
         'discriminator': current_user.id,
     })
 
-@app.route('/logout')
-def logout():
-    logout_user()
-    return 'Logged out'
-
 # @mgr.unauthorized_handler
 # def unauthorized_handler():
 #     return 'Unauthorized', 401
-
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    match request.method:
-        case 'GET':
-            return '''
-                    <form action='register' method='POST'>
-                        <input type='text' name='name' id='name' placeholder='Олег Данилов'/>
-                        <input type='password' name='password' id='password' placeholder='password'/>
-                        <input type='submit' name='submit'/>
-                    </form>
-                    '''
-        case 'POST':
-            name, password = request.form['name'], request.form['password']
-            user = User.query.filter_by(name=name).first()
-            if not user and name and password:
-                user = User(name=name, password=generate_password_hash(password))
-                mgr.db.session.add(user)
-                mgr.db.session.commit()
-                login_user(user)
-                return redirect(url_for('protected'))
-
-    return 'something went wrong'
-
-@app.route('/unregister', methods=['GET', 'POST'])
-@login_required
-def unregister():
-    match request.method:
-        case 'GET':
-            return '''
-                    <h1>U sure?</h1>
-                    <p>Enter your password to confirm</p>
-                    <form action='unregister' method='POST'>
-                        <input type='password' name='password' id='password' placeholder='password'/>
-                        <input type='submit' name='submit'/>
-                    </form>
-                    '''
-        case 'POST':
-            password = request.form['password']
-            if check_password_hash(current_user.password, password):
-                mgr.db.session.delete(current_user)
-                mgr.db.session.commit()
-                logout_user()
-                return redirect('/')
-
-    return '<h1>something went wrong</h1>\n<p>Wrong password?</p>'
-
