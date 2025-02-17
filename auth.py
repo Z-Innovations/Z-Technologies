52ddec6930cf80e3e53032f829ef68ea6f182126
roundedrectangle <kozelderezel12345678@gmail.com>
2025-02-17 23:33:56 +0300

    Olegodanilezation

--- auth.py
+++ auth.py
@@ -1,9 +1,12 @@
 from flask_login import UserMixin, LoginManager, current_user, login_user, login_required, logout_user
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import func
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from flask import Blueprint, request, render_template, redirect, url_for
 from werkzeug.security import check_password_hash, generate_password_hash
 from flask_migrate import Migrate
+
+ONLY_DISCRIMINATOR = False
 
 class Base(DeclarativeBase):
     pass
@@ -15,17 +18,17 @@ class MyLoginManager(LoginManager):
 
         class User(UserMixin, self.db.Model):
             id: Mapped[int] = mapped_column(primary_key=True)
-            name: Mapped[str] # Данил Олегов if empty
+            name: Mapped[str | None] = mapped_column(unique=True) # Олег Данилов if empty
             password: Mapped[str]
-            discriminator: Mapped[int] = mapped_column(nullable=True)
-            creditcard_number: Mapped[int] = mapped_column(nullable=True)
-            creditcard_valid_till: Mapped[str] = mapped_column(nullable=True)
-            creditcard_3_digits: Mapped[int] = mapped_column(nullable=True)
+            discriminator: Mapped[int | None] = mapped_column(unique=True)
+            creditcard_number: Mapped[int | None]
+            creditcard_valid_till: Mapped[str | None]
+            creditcard_3_digits: Mapped[int | None]
         self.User = User
 
         self.bp = Blueprint('auth', __name__, url_prefix='/auth')
-        for f in (self.register, self.unregister, self.login, self.edit_password, self.credit_card):
-            self.register_page(f)
+        for f in 'register unregister login edit_password credit_card switch_to_discriminator'.split():
+            self.register_page(getattr(self, f))
         self.register_page(self.logout, False)
 
         super().__init__(app, add_context_processor)
@@ -46,20 +49,35 @@ class MyLoginManager(LoginManager):
             self.db.create_all()
         self.migrate.init_app(app)
         return super().init_app(app, add_context_processor)
+    
+    @property
+    def next_discriminator(self):
+        return (self.User.query.with_entities(func.max(self.User.discriminator)).scalar() or 0)+1
 
     def register(self):
         match request.method:
             case 'GET':
                 return render_template('auth/login_register.html', mode=0)
             case 'POST':
-                name, password = request.form['name'], request.form['password']
-                user = self.User.query.filter_by(name=name).first()
-                if not user and name and password:
+                password = request.form.get('password', None)
+                if not password:
+                    return render_template('info.html', title="password is empty or not given")
+                discriminator_mode = request.form.get('mode', 'custom' if request.form.get('name', None) else 'oleg') == 'oleg'
+                if ONLY_DISCRIMINATOR and not discriminator_mode:
+                    return render_template('info.html', title="only discriminator (OLEG) mode is allowed in this enivonment")
+                if not discriminator_mode:
+                    name = request.form.get('name', None)
+                    if not name:
+                        return render_template('info.html', title="name empty or not given")
+                    if self.User.query.filter_by(name=name).first():
+                        return render_template('info.html', title="user with this name already exists")
                     user = self.User(name=name, password=generate_password_hash(password))
-                    self.db.session.add(user)
-                    self.db.session.commit()
-                    login_user(user)
-                    return redirect(url_for('protected'))
+                else:
+                    user = self.User(discriminator=self.next_discriminator, password=generate_password_hash(password))
+                self.db.session.add(user)
+                self.db.session.commit()
+                login_user(user)
+                return redirect(url_for('protected'))
         return render_template('info.html', title="something went wrong")
 
     @login_required
@@ -80,8 +98,13 @@ class MyLoginManager(LoginManager):
         if request.method == 'GET':
             return render_template('auth/login_register.html', mode=1)
 
-        name, password = request.form['name'], request.form['password']
-        user = self.User.query.filter_by(name=name).first()
+        password = request.form.get('password', None)
+        if not password:
+            return render_template('info.html', title="No password provided")
+        if not 'name' in request.form and not 'discriminator' in request.form:
+            return render_template('info.html', title="No name or discriminator provided")
+        key = 'name' if request.form.get('name', None) else 'discriminator'
+        user = self.User.query.filter_by(**{key: request.form[key]}).first()
         if user and check_password_hash(user.password, password):
             login_user(user)
             return redirect(url_for('protected'))
@@ -121,4 +144,16 @@ class MyLoginManager(LoginManager):
                 three = request.form['three']
                 self.edit_field(creditcard_number=number, creditcard_valid_till=validtill, creditcard_3_digits=three)
                 return render_template('info.html', title="всё")
+        return render_template('info.html', title="something went wrong", extra="Wrong password?")
+    
+    @login_required
+    def switch_to_discriminator(self):
+        match request.method:
+            case 'GET':
+                return render_template('auth/edit_password.html', switch_to_discriminator=True)
+            case 'POST':
+                password = request.form['password']
+                if check_password_hash(current_user.password, password):
+                    self.edit_field(name=None, discriminator=self.next_discriminator)
+                    return render_template('info.html', title="Success")
         return render_template('info.html', title="something went wrong", extra="Wrong password?")
--- debug.py
+++ debug.py
@@ -1,5 +1,8 @@
+# docker run  -p 5432:5432 -e POSTGRES_PASSWORD=postgres_password -e POSTGRES_USER=postgres_user -e POSTGRES_DB=postgres postgres:latest
+
 import os
-os.environ['DATABASE_URI'] = 'sqlite:///users.db'
+# os.environ['DATABASE_URI'] = 'sqlite:///users.db'
+os.environ['DATABASE_URI'] = 'postgresql://postgres_user:postgres_password@localhost/postgres'
 os.environ['SECRET_KEY'] = 'obviously_not_secret_key'
 os.environ['DEBUG'] = 'True'
 
